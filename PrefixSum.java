d000d51917ab3628d6ef8cbb450492437c0968d5
Heerali-G <heerajain0404@gmail.com>
2025-02-09 12:15:31 +0530

    Create PrefixSum.java

---
+++ PrefixSum.java
@@ -0,0 +1,21 @@
+//You work as a software engineer for a company that manages online retail stores. The company wants to implement a feature that provides real-time insights into daily sales trends.
+//One of the key metrics is the cumulative sales amount up to a specific hour of the day.
+import java.util.Arrays;
+public class PrefixSum
+{
+    public static void main(String[] args)
+    {
+        int[] sales = {100, 150, 200, 50, 250};
+        int[] prefixSum = new int[sales.length];
+        if (sales.length > 0)
+        {
+            prefixSum[0] = sales[0];
+            for (int i = 1; i < sales.length; i++)
+            {
+                prefixSum[i] = prefixSum[i - 1] + sales[i];
+            }
+        }
+        System.out.println("Sales: "+Arrays.toString(sales));      
+        System.out.println("PrefixSum: " +Arrays.toString(prefixSum));
+    }
+}
