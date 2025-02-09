d27208b60d01ada8f42a3d3f906120f62aa93a02
Heerali-G <heerajain0404@gmail.com>
2025-02-09 12:30:05 +0530

    PrefixSum.java

--- PrefixSum.java
+++ PrefixSum.java
@@ -7,15 +7,19 @@ public class PrefixSum
     {
         int[] sales = {100, 150, 200, 50, 250};
         int[] prefixSum = new int[sales.length];
+        System.out.println("Sales: "+Arrays.toString(sales));
+        System.out.println("The prefix sums are calculated as: ");
         if (sales.length > 0)
         {
             prefixSum[0] = sales[0];
+             System.out.println("i=0: " + prefixSum[0]);
             for (int i = 1; i < sales.length; i++)
             {
                 prefixSum[i] = prefixSum[i - 1] + sales[i];
+                System.out.println("i="+ i +": "+prefixSum[i-1]+ " + "+sales[i]+ " = "+prefixSum[i]);
             }
         }
-        System.out.println("Sales: "+Arrays.toString(sales));      
+              
         System.out.println("PrefixSum: " +Arrays.toString(prefixSum));
     }
 }
